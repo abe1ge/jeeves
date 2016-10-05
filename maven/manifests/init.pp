71b50c58f9c7e9182b6ef836f4b33556847be0bd
HenriQA <henri.william@qa.com>
2016-10-05 15:49:05 +0000

    maven_files

---
+++ maven/manifests/init.pp
@@ -0,0 +1,23 @@
+class maven {
+	
+	Exec {
+		path => ["/usr/bin", "/bin", "/usr/sbin"],
+	}
+	
+	file { '/opt/maven.tar.gz':
+		ensure => 'present',
+		source => 'puppet:///modules/maven/maven.tar.gz',
+	}
+
+	exec { 'extract maven':
+		cwd => '/opt',
+		command => 'sudo tar -zxvf maven.tar.gz',
+		require => File['/opt/maven.tar.gz'],
+	}
+
+	exec { 'install maven':
+		require => Exec['extract maven'],
+		cwd => '/opt',
+		command => 'sudo update-alternatives --install /usr/bin/mvn mvn /opt/apache-maven-3.3.9/bin/mvn 100',
+	}
+}
