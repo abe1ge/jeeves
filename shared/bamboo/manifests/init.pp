2bc34b703b925be9d8b7beab2ed676d9a9b5066f
s-irvine <sam_irvine@hotmail.co.uk>
2016-10-06 16:21:06 +0100

    Bamboo init file

---
+++ shared/bamboo/manifests/init.pp
@@ -0,0 +1,39 @@
+class bamboo {
+
+	Exec {
+		path => ["/usr/bin", "/bin", "/usr/sbin"],
+	}
+
+	file { '/opt/atlassian-bamboo-5.13.2.tar.gz':
+		ensure => 'present',
+		source => 'puppet:///modules/bamboo/atlassian-bamboo-5.13.2.tar.gz',
+	}
+
+	exec { 'extract bamboo files':
+		cwd => '/opt/',
+		command => 'sudo tar -zxvf atlassian-bamboo-5.13.2.tar.gz',
+		require => File['/opt/atlassian-bamboo-5.13.2.tar.gz'],
+	}
+
+	exec { 'make bamboo home directory':
+		command => 'mkdir -p /home/vagrant/bamboo/bamboo-home',
+	}
+
+	exec { 'change permissions for bamboo-init':
+		cwd => '/opt/atlassian-bamboo-5.13.2/atlassian-bamboo/WEB-INF/classes',
+		command => 'sudo chmod 777 bamboo-init.properties',
+		require => Exec['extract bamboo files', 'make bamboo home directory'],
+	}
+
+	exec { 'edit bamboo-init.properties':
+		command => 'sudo echo "bamboo.home=/home/vagrant/bamboo/bamboo-home" >> bamboo-init.properties',
+		require => Exec['change permissions for bamboo-init'],
+	}
+
+	exec { 'start bamboo':
+		cwd => '/opt/atlassian-bamboo-5.13.2',
+		command => 'sudo bin/start-bamboo.sh',
+		require => Exec['edit bamboo-init.properties'],
+	}
+
+}
