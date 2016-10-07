aff1143dcdffd177d2031fdfea9d76ab471dacae
TimPerry1994 <timperry@fsmail.net>
2016-10-07 09:09:07 +0100

    Changed install.pp to work with agents

--- shared/jira/files/jira_setup.sh
+++ shared/jira/files/jira_setup.sh
@@ -1,1 +1,1 @@
-(echo "o"; echo "2"; echo "/opt/atlassian/jira"; echo "/var/atlassian/application-data/jira";echo "2"; echo "8081"; echo "8006"; echo "y")|sudo /etc/puppet/modules/jira/files/jira.bin
+(echo "o"; echo "2"; echo "/opt/atlassian/jira"; echo "/var/atlassian/application-data/jira";echo "2"; echo "8081"; echo "8006"; echo "y")|sudo /home/vagrant/jira.bin
