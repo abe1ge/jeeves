77657ebb4d12c08d99617da8fcb29d6e43fdada2
TimPerry1994 <timperry@fsmail.net>
2016-10-07 10:10:09 +0100

    Created bamboo init

--- shared/jira/files/jira_setup.sh
+++ shared/jira/files/jira_setup.sh
@@ -1,1 +1,1 @@
-(echo "o"; echo "2"; echo "/opt/atlassian/jira"; echo "/var/atlassian/application-data/jira";echo "2"; echo "8081"; echo "8006"; echo "y")|sudo /home/vagrant/jira.bin
+(echo "o"; echo "2"; echo "/opt/atlassian/jira"; echo "/var/atlassian/application-data/jira";echo "2"; echo "8082"; echo "8007"; echo "y")|sudo /home/vagrant/jira.bin
