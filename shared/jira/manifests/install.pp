7a6011fae083e4fb9600cbda4a748d0fc6dc17ac
HenriQA <henri.william@qa.com>
2016-10-10 12:38:59 +0100

    Changes to be committed: 	modified:   shared/bamboo/manifests/init.pp 	modified:   shared/jira/manifests/install.pp
     Untracked files:
    	shared/bash.sh
    	shared/spam.sh

--- shared/jira/manifests/install.pp
+++ shared/jira/manifests/install.pp
@@ -3,25 +3,30 @@ class jira::install {
 	file { '/opt/jira.bin':
 		ensure => present,
 		source => '/tmp/shared/jira.bin',
+		notify => Exec['make_executable'],
+	}
+	
+	exec { 'make_executable':
+		path => ['/bin','/usr/bin'],
+		command => 'sudo chmod a+x /opt/jira.bin',
+		refreshonly => 'true',
+		require => File['/opt/jira.bin'],
+		notify => File['/opt/jira_setup.sh'],
 	}
 
 	file { '/opt/jira_setup.sh':
 		ensure => present,
 		source => '/tmp/shared/jira/files/jira_setup.sh',
-		before => Exec['unzip_jira'],
+		refreshonly => 'true'
+		notify => Exec['unzip_jira'],
 	}
 
 	exec { 'unzip_jira':
 		unless => 'grep -rnw "/opt/atlassian/jira" -e "install"',
 		path => ['/bin','/usr/bin'],
 		command => 'sudo bash /opt/jira_setup.sh',
+		refreshonly => 'true',
 		require => Exec['make_executable'],
-	}
-	
-	exec { 'make_executable':
-		path => ['/bin','/usr/bin'],
-		command => 'sudo chmod a+x /opt/jira.bin',
-		require => File['/opt/jira.bin'],
-	}
-	
+		require => File['/opt/jira_setup.sh'],
+	}	
 }
