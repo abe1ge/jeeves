18cfaba3fde2c078559250bc58a2cece576f7a6d
tommitchell468 <tom.mitchell@qa.com>
2016-10-12 14:14:38 +0100

    MySQL finesse update
    I removed the forced passing of certain lines and the reliance on an external bash script

--- shared/mysqlinstall/manifests/init.pp
+++ shared/mysqlinstall/manifests/init.pp
@@ -1,13 +1,10 @@
 class mysqlinstall {
 
-require javainstall
-#require maven
-#require git
-#require jira
-
 Exec {
 	path => ["/usr/bin", "/bin", "/usr/sbin","/sbin"],
 }
+
+$frontend="DEBIAN_FRONTEND='noninteractive'"
 
 file { '/opt/mysql-server_5.7.15-1ubuntu14.04_amd64.deb-bundle.tar': 
 ensure => 'present',
@@ -18,60 +15,68 @@ notify => Exec['Extract sql'],
 exec { 'Extract sql':
 cwd => '/opt',
 command => 'sudo tar -xvf /opt/mysql-server_5.7.15-1ubuntu14.04_amd64.deb-bundle.tar',
-#logoutput => true,
+logoutput => true,
 refreshonly => true,
 notify => Exec['sqlprecon'],
 }
 
 exec { 'sqlprecon':
-command => 'sudo bash /tmp/shared/mysqlinstall/files/precon.sh',
-#logoutput => true,
+command => "sudo ${frontend} dpkg-preconfigure mysql-community-server_5.7.15-1ubuntu14.04_amd64.deb",
+logoutput => true,
 cwd => '/opt',
 refreshonly => true,
 notify => Exec['sqlclients'],
 }
 
+#exec { 'sqlprecon':
+#command => 'sudo bash /tmp/shared/mysqlinstall/files/precon.sh',
+#logoutput => true,
+#cwd => '/opt',
+#refreshonly => true,
+#notify => Exec['sqlclients'],
+#}
+
 exec { 'sqlclients':
-command => 'sudo dpkg -i mysql-common_5.7.15-1ubuntu14.04_amd64.deb || :',
+command => 'sudo dpkg -i mysql-common_5.7.15-1ubuntu14.04_amd64.deb',
 cwd => '/opt',
 provider => shell,
-#logoutput => true,
+logoutput => true,
 refreshonly => true,
 notify => Exec['sqlclients2'],
 }
 
 exec { 'sqlclients2':
-command => 'sudo dpkg -i mysql-community-client_5.7.15-1ubuntu14.04_amd64.deb || :',
+command => 'sudo dpkg -i mysql-community-client_5.7.15-1ubuntu14.04_amd64.deb',
 cwd => '/opt',
 provider => shell,
-#logoutput => true,
+logoutput => true,
 refreshonly => true,
 notify => Exec['sqlclients3'],
 }
 
 exec { 'sqlclients3':
-command => 'sudo dpkg -i mysql-client_5.7.15-1ubuntu14.04_amd64.deb || :',
+command => 'sudo dpkg -i mysql-client_5.7.15-1ubuntu14.04_amd64.deb',
 cwd => '/opt',
 provider => shell,
-#logoutput => true,
+logoutput => true,
 refreshonly => true,
 notify => Exec['sqlservers'],
 }
 
 
 exec { 'sqlservers':
-command => 'sudo dpkg -i mysql-community-server_5.7.15-1ubuntu14.04_amd64.deb || :',
+command => 'sudo dpkg -i mysql-community-server_5.7.15-1ubuntu14.04_amd64.deb',
 cwd => '/opt',
-#logoutput => true,
+logoutput => true,
 refreshonly => true,
 provider => shell,
 notify => Exec['sqlservers2'],
 }
 
 exec { 'sqlservers2':
-command => 'sudo dpkg -i mysql-server_5.7.15-1ubuntu14.04_amd64.deb ||:',
+command => 'sudo dpkg -i mysql-server_5.7.15-1ubuntu14.04_amd64.deb',
 cwd => '/opt',
-#logoutput => true,
+logoutput => true,
 provider => shell,
 refreshonly => true,
 }
