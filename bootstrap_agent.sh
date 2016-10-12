9225373ac0ad6be760ede2ec123cba4d7d177313
s-irvine <sam_irvine@hotmail.co.uk>
2016-10-12 15:12:59 +0100

    Fancy bootstrap master ip and fqdn update

--- bootstrap_agent.sh
+++ bootstrap_agent.sh
@@ -5,12 +5,14 @@ sudo apt-get -y update
 
 echo "Box updated" 
 
+#Install openssh and sshPass
 sudo apt-get -y install openssh-server openssh-client
 
 sudo apt-get install sshpass
 
 echo "Open ssh installed"
 
+#Installing dependencies for the MySQL module
 sudo apt-get install -y libaio1
 sudo apt-get install -y libmecab2
 
@@ -20,21 +22,25 @@ sudo ufw disable
 
 echo "Firewall disabled"
 
-#Change timezone and keyboard
+#Change time-zone
 
 sudo timedatectl set-timezone Europe/London
-setxkbmap gb
 
-echo "Time Zone and Keyboard set to UK standard"
+echo "Time-Zone set to UK standard"
 
 #Install Puppet
 sudo apt-get install -y puppet
 
 echo "Puppet installed"
 
-mfqdn="jeevesmaster.qac.local"
-mip="192.168.1.80"
+#Getting the ip and fqdn from of the master from the file the master 
+#bootstrap saved and saving them as $mip and $mfqdn respectively
+. /tmp/shared/mfqdn.file
+. /tmp/shared/mip.file
 
+echo "The master ip is $mip and its' FQDN is $mfqdn"
+
+#Updating hosts file
 fqdn=`facter fqdn`
 ip=`facter ipaddress`
 
@@ -46,11 +52,13 @@ sed -i "2iserver=$mfqdn" /etc/puppet/puppet.conf
 
 echo "Hosts file updated"
 
+#Retuning time taken to this point
 duration1=$SECONDS
 echo "$(($duration1 / 60)) minute(s) and $(($duration1 % 60)) second(s) elapsed."
 
 sudo puppet agent --test --server="$mfqdn"
 
+#SSH into master to approve puppet certificate
 sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@"$mip" << EOF
 sudo puppet cert list
 sudo puppet cert sign --all
@@ -59,20 +67,25 @@ EOF
 
 echo " Puppet certificate signed"
 
+#Restarting puppet service
 sudo service puppet stop
 sudo service puppet start
 
 echo "Puppet service restarted"
 
+#Pause to allow puppet service to restart fully
 sleep 3
 
+#Enable puppet agent
 sudo puppet agent --enable
 
 echo "Puppet agent enabled"
 
+#Contacting puppet master and provisioning the modules
 sudo puppet agent --test --server="$mfqdn"
 
 echo "Puppet agent has linked to master server and applied the puppet modules"
 
+#Returning the total time to provision the box
 duration2=$SECONDS
 echo "$(($duration2 / 60)) minute(s) and $(($duration2 % 60)) seconds elapsed."
--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -5,36 +5,55 @@ sudo apt-get -y update
 
 echo "Box updated"  
 
+#Install open SSH
 sudo apt-get -y install openssh-server openssh-client
 
 echo "Open ssh installed"
 
+#Disable Firewall
 sudo ufw disable
 
 echo "Firewall disabled"
 
-#Set time-zone and keyboard
-
+#Set time-zone
 sudo timedatectl set-timezone Europe/London
-setxkbmap gb
 
-echo "Time Zone and Keyboard set to UK standard"
+echo "Time-Zone set to UK standard"
 
 #Install Puppet
 sudo apt-get -y install puppet puppetmaster
 
 echo "Puppet installed"
 
+#Getting master fqdn and ip
 fqdn=`facter fqdn`
 ip=`facter ipaddress_eth1`
 
+# Check if mip or mfqdn files exist and deleting them if they do
+if [ -e /tmp/shared/mfqdn.file ]
+then
+	sudo rm /tmp/shared/mfqdn.file
+fi
+
+if [ -e /tmp/shared/mip.file ]
+then
+	sudo rm /tmp/shared/mip.file
+fi
+
+#Saving the ip and fqdn of the master
+echo 'mfqdn="'"$fqdn"'"' >> /tmp/shared/mfqdn.file
+echo 'mip="'"$ip"'"' >> /tmp/shared/mip.file
+
+#Updating the hosts file
 sed -i "1i127.0.0.1	$fqdn	puppetmaster" /etc/hosts
 sed -i "2i$ip	$fqdn	puppetmaster" /etc/hosts
 
 echo "Hosts file updated"
 
+#Creating Site.pp
 sudo touch /etc/puppet/manifests/site.pp
 
+#Copying module directories to a default path
 sudo cp -r /tmp/shared/bamboo /usr/share/puppet/modules
 sudo cp -r /tmp/shared/git /usr/share/puppet/modules
 sudo cp -r /tmp/shared/javainstall /usr/share/puppet/modules
@@ -47,6 +66,7 @@ sudo cp -r /tmp/shared/zabbix /usr/share/puppet/modules
 
 echo "Modules Copied"
 
+#Updating Site.pp so puppet provisions the correct modules for each agent
 sudo echo "node 'jeevesagent1.qac.local','jeevesagent2.qac.local','jeevesagent3.qac.local','jeevesagent4.qac.local' {" >> /etc/puppet/manifests/site.pp
 sudo echo "include javainstall" >> /etc/puppet/manifests/site.pp
 sudo echo "include maven" >> /etc/puppet/manifests/site.pp
@@ -64,5 +84,6 @@ sudo echo "}" >> /etc/puppet/manifests/site.pp
 
 echo "Site.pp updated"
 
+#Returning the time taken for the script to run
 duration=$SECONDS
 echo "$(($duration / 60)) minute(s) and $(($duration % 60)) second(s) elapsed."
